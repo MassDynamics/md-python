77a57f2b38e39ef954aeecf3beb13e3b843fe01d
Giuseppe Infusini <infusini@gmail.com>
2025-09-21 17:13:06 -0500

    simplify MDClient initialization by using logical OR for base_url and api_token assignment

--- src/md_python/client.py
+++ src/md_python/client.py
@@ -17,18 +17,14 @@ load_dotenv()
 class MDClient:
     """Enhanced MD Client that combines simplicity with type safety"""
 
-    base_url: str #= "https://app.massdynamics.com/api"
+    base_url: str  # Default base URL
     api_token: str
 
     def __init__(self, api_token: Optional[str] = None, base_url: Optional[str] = None):
         
-        self.base_url = base_url
-        if not self.base_url:
-            self.base_url = os.getenv("MD_API_BASE_URL")
+        self.base_url = base_url or os.getenv("MD_API_BASE_URL")
 
-        self.api_token = api_token
-        if not self.api_token:
-            self.api_token = os.getenv("MD_AUTH_TOKEN")
+        self.api_token = api_token or os.getenv("MD_AUTH_TOKEN")
 
         # Nested resource structure
         self.health = Health(self)
